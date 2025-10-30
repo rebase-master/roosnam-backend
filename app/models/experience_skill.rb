276d28d0243b1194193630322a6e0b54fba941c8
Mansoor Khan <mansoor@incubyte.co>
2025-10-30 22:59:34 +0530

    Add RAILS_MATER_KEY to docker file
    Fix enum issue as per Rails 8.1

--- app/models/experience_skill.rb
+++ app/models/experience_skill.rb
@@ -2,11 +2,12 @@ class ExperienceSkill < ApplicationRecord
   belongs_to :skill
   belongs_to :company_experience
 
-  enum proficiency_level: {
-    beginner: 'Beginner',
+  enum :proficiency_level, {
+    beginner:  'Beginner',
     intermediate: 'Intermediate',
-    advanced: 'Advanced'
-  }, _suffix: true, _scopes: true
+    advanced:  'Advanced'
+  }, suffix: true, scopes: true, enum_type: :string
+
 
   validates :years_of_experience, numericality: { allow_nil: true, greater_than_or_equal_to: 0 }
 end
