b5c5fe07f43bc5aa5df0e46a2d6c4f176758cea6
Mansoor Khan <mansoor@incubyte.co>
2025-11-09 23:33:15 +0530

    Use ActiveStorage
    Fix issues with Docker files
    
    Modify Rails Admin views

--- app/models/attachment.rb
+++
@@ -1,9 +0,0 @@
-class Attachment < ApplicationRecord
-  belongs_to :attachable, polymorphic: true
-
-  validates :file_url, presence: true
-  validates :file_type, inclusion: {
-    in: %w[image/png image/jpeg image/jpg image/webp application/pdf],
-    message: "%{value} is not a supported format"
-  }, allow_blank: true
-end
