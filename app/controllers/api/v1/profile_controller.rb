546b307d5df1833942d9af4433ddd9833852bb3c
Mansoor Khan <mansoor@incubyte.co>
2025-11-28 14:07:50 +0530

    Refactoring and cleanup

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -2,7 +2,10 @@ module Api
   module V1
     class ProfileController < BaseController
       def show
-        render json: portfolio_user, serializer: UserProfileSerializer
+        render json: portfolio_user
+      rescue Exception => e
+        Rails.logger.error("API ERROR: An error occurred: #{e.message}")
+        render json: { status: :not_found, error: "Internal server error" }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :profile, only: [:show], controller: 'profile'
-      resources :works, only: [:index]
       resources :client_projects, only: [:index]
       resources :client_reviews, only: [:index]
       resources :work_experiences, only: [:index]
-      resources :experience_skills, only: [:index]
       resources :certifications, only: [:index]
       resources :education, only: [:index]
     end
